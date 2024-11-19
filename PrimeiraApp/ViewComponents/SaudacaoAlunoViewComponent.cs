42f2021c83aa55aa431fa81e0f56d249c252c1a8
ronaldo <ronaldo.dsantos@gmail.com>
2024-11-19 00:34:49 -0300

    add views e razor

---
+++ PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // poderiamos estar pegando os dados do aluno no banco de dados
+            // poderiamos estar pegando os dados do aluno que está logado
+            var aluno = new Aluno() { Nome = "Ronaldo" };
+
+            return View(aluno);
+        }
+
+    }
+}
--- PrimeiraApp/obj/Debug/net8.0/PrimeiraApp.AssemblyInfo.cs
+++ PrimeiraApp/obj/Debug/net8.0/PrimeiraApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiraApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8f6cca5d5f56103de19748a3667b7677fb7d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiraApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiraApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
