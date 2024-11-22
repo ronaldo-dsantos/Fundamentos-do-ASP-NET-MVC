fd7ca732b3713871d23b4f13b4c0b4a6578f0d17
ronaldo <ronaldo.dsantos@gmail.com>
2024-11-21 23:30:15 -0300

    add formularios

---
+++ PrimeiraApp/Controllers/AlunosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AlunosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")]Aluno aluno) // Estamos utilizando o Bind por questão de segurança, para informar apenas o nome dos campos que queremos receber
+        {
+            _context.Alunos.Add(aluno);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index)); // Redirecionando para a controller index
+        }
+
+
+    }
+}
