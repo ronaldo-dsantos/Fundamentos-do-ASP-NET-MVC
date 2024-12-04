030df149b80708eebc03254dc2529377799d38cd
ronaldo <ronaldo.dsantos@gmail.com>
2024-12-04 15:54:51 -0300

    add comments models

--- PrimeiraApp/Controllers/ModelsController.cs
+++ PrimeiraApp/Controllers/ModelsController.cs
@@ -3,12 +3,12 @@ using PrimeiraApp.Models;
 
 namespace PrimeiraApp.Controllers
 {
-    // classe criada para validação do modelo de dados criado na classe Aluno
+    // Controller criada para validação do modelo de dados criado na classe Aluno, este exemplo não é necessário se os dados estiverem vindo de um formulário, neste caso a validação é automática pelo data bind
     public class ModelsController : Controller
     {
         public IActionResult Index() 
         {
-            //var aluno = new Aluno();
+            // var aluno = new Aluno();
 
             var aluno = new Aluno()
             {
@@ -18,21 +18,22 @@ namespace PrimeiraApp.Controllers
             };
 
             // método para validar um modelo de dados
-            if (TryValidateModel(aluno)) 
+            if (TryValidateModel(aluno)) // TryValidateModel para verificar se a model aluno está válida
             {
                 return View(aluno);
             }
 
-            var ms = ModelState;
+            // Armazenando os dados da ModelState em uma variável
+            var ms = ModelState; 
 
-            // tratando os erros para que possamos visualisá-los no console
+            // Armazenando os erros do ModelState em uma lista
             var erros = ModelState.Select(x => x.Value.Errors)
                                     .Where(y => y.Count > 0)
                                     .ToList();
-
+            
+            // Exibindo a lista de erros no console
             erros.ForEach(r => Console.WriteLine(r.First().ErrorMessage));
             
-
             return View(); 
         }
     }
--- PrimeiraApp/Models/Aluno.cs
+++ PrimeiraApp/Models/Aluno.cs
@@ -5,32 +5,32 @@ namespace PrimeiraApp.Models
 {
     public class Aluno
     {
-        [Key] // mapeando a chave primária, desta forma irá gerar no banco de dados e não no formulário
+        [Key] // [Key] = Para mapear a chave primária, desta forma irá gerar no banco de dados e não no formulário
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")] // parametro {0} referece ao nome do campo
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")] // [StringLength] = Ramanho do campo
         public string? Nome { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto")]
-        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")] // [Required] = Campo obrigatório
+        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto")] // [DataType] = Tipo de dado
+        [Display(Name = "Data de Nascimento")] // [Display] = Como o nome do campo vai ser exibido na aplicação
         public DateTime DataNascimento { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")] 
         [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
-        //[RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "O campo {0} está em formato inválido")]
-        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        //[RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "O campo {0} está em formato inválido")] // [RegularExpression] = Expressão regular para validar e-mail
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")] // [EmailAddress] = Para validação de e-mail
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [Display(Name = "Confirme o e-mail")]
-        [Compare("Email", ErrorMessage = "Os e-mails informados não são identicos")]
-        [NotMapped] // não mapear esse campo no banco de dados, apenas no formulário
+        [Compare("Email", ErrorMessage = "Os e-mails informados não são identicos")] // [Compare] = Para comparação de valores
+        [NotMapped] // [NotMapped] = Para não mapear esse campo no banco de dados, apenas no formulário
         public string? EmailConfirmacao { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")] 
-        [Range(1, 5, ErrorMessage = "O campo {0} deve estar entre {1} e {2}")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve estar entre {1} e {2}")] // [Range] = Range de valores permitidos
         public int Avaliacao { get; set; }
 
         public bool Ativo { get; set; }
