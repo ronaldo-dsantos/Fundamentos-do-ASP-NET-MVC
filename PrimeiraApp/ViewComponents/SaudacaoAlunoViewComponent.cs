4e9e7db6ae309d886517395d609ef881507d6eba
ronaldo <ronaldo.dsantos@gmail.com>
2024-12-04 22:37:25 -0300

    add comments views

--- PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
+++ PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// Exemplo de View Components (diferente das partial views, ele é um componente independente das Views e tem um processamento individual)
+
+using Microsoft.AspNetCore.Mvc;
 using PrimeiraApp.Models;
 
 namespace PrimeiraApp.ViewComponents
@@ -7,6 +9,7 @@ namespace PrimeiraApp.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            // podemos realizar um processamento independente da controller
             // poderiamos estar pegando os dados do aluno no banco de dados
             // poderiamos estar pegando os dados do aluno que está logado
             var aluno = new Aluno() { Nome = "Ronaldo" };
